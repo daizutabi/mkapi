849df0e98af74fa1e4dddf9b4a3039dd03e0bf51
daizu <daizutabi@gmail.com>
2020-06-19 19:47:55 +0900

    Delete click command.

--- mkapi/main.py
+++ mkapi/main.py
@@ -1,21 +1,4 @@
-import os
-import sys
-
-import click
 from markdown import Markdown
-
-from mkapi import __version__
-
-pgk_dir = os.path.dirname(os.path.abspath(__file__))
-version_msg = f"{__version__} from {pgk_dir} (Python {sys.version[:3]})."
-
-
-@click.group(invoke_without_command=True)
-@click.pass_context
-@click.version_option(version_msg, "-V", "--version")
-def cli(ctx):
-    pass
-
 
 converter = Markdown()
 
