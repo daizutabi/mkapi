15c21f3ec0b7fd8c9ff39103190132273d92081e
daizutabi <daizutabi@gmail.com>
2024-02-27 06:59:36 +0900

    nodes.py

--- src/mkapi/nodes.py
+++ src/mkapi/nodes.py
@@ -2,18 +2,11 @@
 from __future__ import annotations
 
 import ast
-import importlib
 from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
 import mkapi.ast
-from mkapi.utils import (
-    cache,
-    get_module_name,
-    get_module_node,
-    is_package,
-    iter_attribute_names,
-)
+from mkapi.utils import get_module_node, is_package, iter_attribute_names
 
 try:
     from ast import TypeAlias
@@ -60,15 +53,15 @@ class Module(Node):
 def _iter_nodes(node: ast.AST, module: str) -> Iterator[Object | Import]:
     for child in mkapi.ast.iter_child_nodes(node):
         if isinstance(child, ast.Import):
-            for name, fullname in _iter_imports_from_import(child):
+            for name, fullname in _iter_imports(child):
                 yield Import(name, child, module, fullname)
 
         elif isinstance(child, ast.ImportFrom):
             if child.names[0].name == "*":
-                yield from _iter_members_from_star(child, module)
+                yield from _iter_imports_from_star(child, module)
 
             else:
-                it = _iter_imports_from_import_from(child, module)
+                it = _iter_imports_from(child, module)
                 for name, fullname in it:
                     yield Import(name, child, module, fullname)
 
@@ -80,7 +73,7 @@ def _iter_nodes(node: ast.AST, module: str) -> Iterator[Object | Import]:
             yield Object(child.name, child, module)
 
 
-def _iter_imports_from_import(node: ast.Import) -> Iterator[tuple[str, str]]:
+def _iter_imports(node: ast.Import) -> Iterator[tuple[str, str]]:
     for alias in node.names:
         if alias.asname:
             yield alias.asname, alias.name
@@ -90,7 +83,7 @@ def _iter_imports_from_import(node: ast.Import) -> Iterator[tuple[str, str]]:
                 yield module_name, module_name
 
 
-def _get_module_from_import_from(node: ast.ImportFrom, module: str) -> str:
+def _get_module(node: ast.ImportFrom, module: str) -> str:
     if not node.module:
         return module
 
@@ -108,25 +101,25 @@ def _get_module_from_import_from(node: ast.ImportFrom, module: str) -> str:
     return f"{prefix}.{node.module}"
 
 
-def _iter_imports_from_import_from(
+def _iter_imports_from(
     node: ast.ImportFrom,
     module: str,
 ) -> Iterator[tuple[str, str]]:
-    module = _get_module_from_import_from(node, module)
+    module = _get_module(node, module)
     for alias in node.names:
         yield alias.asname or alias.name, f"{module}.{alias.name}"
 
 
-def _iter_members_from_star(
+def _iter_imports_from_star(
     node: ast.ImportFrom,
     module: str,
 ) -> Iterator[Object | Import]:
-    module = _get_module_from_import_from(node, module)
+    module = _get_module(node, module)
     if node_ := get_module_node(module):
         yield from _iter_nodes(node_, module)
 
 
-def _resolve(fullname: str) -> Iterator[Module | Object | Import]:
+def resolve(fullname: str) -> Iterator[Module | Object | Import]:
     if node := get_module_node(fullname):
         yield Module(fullname, node)
         return
@@ -148,17 +141,17 @@ def _resolve(fullname: str) -> Iterator[Module | Object | Import]:
             elif member.fullname == fullname:
                 continue
 
-            yield from _resolve(member.fullname)
+            yield from resolve(member.fullname)
 
 
-def _parse(node: ast.Module, module: str) -> list[tuple[str, Module | Object | Import]]:
+def parse(node: ast.Module, module: str) -> list[tuple[str, Module | Object | Import]]:
     members = []
 
     for member in _iter_nodes(node, module):
         name = member.name
 
         if isinstance(member, Import):
-            if resolved := list(_resolve(member.fullname)):
+            if resolved := list(resolve(member.fullname)):
                 members.extend((name, x) for x in resolved)
             else:
                 members.append((name, member))
@@ -167,133 +160,3 @@ def _parse(node: ast.Module, module: str) -> list[tuple[str, Module | Object | I
             members.append((name, member))
 
     return members
-
-
-@cache
-def parse(module: str) -> list[tuple[str, Module | Object | Import]]:
-    if node := get_module_node(module):
-        return _parse(node, module)
-
-    return []
-
-
-def get_all_names(module: str) -> list[str]:
-    members = importlib.import_module(module).__dict__
-
-    members_all = members.get("__all__")
-    if isinstance(members_all, list | tuple):
-        return list(members_all)
-
-    return []
-
-
-def _split_fullname(obj: Module | Object | Import) -> tuple[str, str | None]:
-    if isinstance(obj, Module):
-        return get_module_name(obj.name), None
-
-    if isinstance(obj, Object):
-        module = get_module_name(obj.module)
-        return module, obj.name
-
-    return obj.fullname, None  # import
-
-
-def _get_fullname(obj: Module | Object | Import) -> str:
-    module, name = _split_fullname(obj)
-    if name is None:
-        return module
-
-    return f"{module}.{name}"
-
-
-def resolve_module_name(name: str) -> tuple[str, str | None] | None:
-    if resolved := list(_resolve(name)):
-        return _split_fullname(resolved[0])
-
-    if "." not in name:
-        return None
-
-    name, attr = name.rsplit(".", maxsplit=1)
-
-    if resolved := resolve_module_name(name):
-        module, name_ = resolved
-        name = f"{name_}.{attr}" if name_ else attr
-        return module, name
-
-    return None
-
-
-def resolve(name: str) -> str | None:
-    if module_name := resolve_module_name(name):
-        module, name_ = module_name
-        if name_ is None:
-            return module
-
-        return f"{module}.{name_}"
-
-    return None
-
-
-def resolve_from_module(name: str, module: str) -> str | None:
-    if name.startswith(module) or module.startswith(name):
-        return name
-
-    for name_, obj in parse(module):
-        if name_ == name:
-            return _get_fullname(obj)
-
-    if name in get_all_names(module):
-        return f"{module}.{name}"
-
-    if "." not in name:
-        return None
-
-    name, attr = name.rsplit(".", maxsplit=1)
-
-    for name_, obj in parse(module):
-        if name_ == name:
-            if isinstance(obj, Module):
-                return resolve(f"{obj.name}.{attr}")
-
-            return f"{_get_fullname(obj)}.{attr}"
-
-    return None
-
-
-# def split_module_name(name: str) -> tuple[str, str | None] | None:
-#     for module in iter_attribute_names(name):
-#         if not get_module_node(module):
-#             continue
-
-#         if module == name:
-#             return name, None
-
-#         name_ = name[len(module)+1:]
-
-#         if
-
-
-def iter_decorator_names(node: ast.AST, module: str) -> Iterator[str]:
-    """Yield decorator_names."""
-    if not isinstance(node, ast.ClassDef | ast.FunctionDef | ast.AsyncFunctionDef):
-        return
-
-    for deco in node.decorator_list:
-        deco_name = next(mkapi.ast.iter_identifiers(deco))
-
-        if name := resolve_from_module(deco_name, module):
-            yield name
-
-        else:
-            yield deco_name
-
-
-def has_decorator(node: ast.AST, name: str, module: str) -> bool:
-    """Return a decorator expr by name."""
-    it = iter_decorator_names(node, module)
-    return any(deco_name == name for deco_name in it)
-
-
-def is_dataclass(node: ast.AST, module: str) -> bool:
-    """Return True if the [Class] instance is a dataclass."""
-    return has_decorator(node, "dataclasses.dataclass", module)
--- tests/test_nodes.py
+++ tests/test_nodes.py
@@ -2,25 +2,19 @@ import ast
 
 from mkapi.nodes import (
     Object,
-    _get_fullname,
-    _iter_imports_from_import,
-    _iter_imports_from_import_from,
-    _parse,
-    get_all_names,
-    has_decorator,
-    iter_decorator_names,
+    _iter_imports,
+    _iter_imports_from,
     parse,
     resolve,
-    resolve_from_module,
-    resolve_module_name,
 )
+from mkapi.utils import get_module_node
 
 
 def test_iter_import_asname():
     src = "import matplotlib.pyplot"
     node = ast.parse(src).body[0]
     assert isinstance(node, ast.Import)
-    x = list(_iter_imports_from_import(node))
+    x = list(_iter_imports(node))
     assert len(x) == 2
     for i in [0, 1]:
         assert x[0][i] == "matplotlib"
@@ -28,119 +22,25 @@ def test_iter_import_asname():
     src = "import matplotlib.pyplot as plt"
     node = ast.parse(src).body[0]
     assert isinstance(node, ast.Import)
-    x = list(_iter_imports_from_import(node))
+    x = list(_iter_imports(node))
     assert len(x) == 1
     assert x[0][0] == "plt"
     assert x[0][1] == "matplotlib.pyplot"
     src = "from matplotlib import pyplot as plt"
     node = ast.parse(src).body[0]
     assert isinstance(node, ast.ImportFrom)
-    x = list(_iter_imports_from_import_from(node, ""))
+    x = list(_iter_imports_from(node, ""))
     assert len(x) == 1
     assert x[0][0] == "plt"
     assert x[0][1] == "matplotlib.pyplot"
 
 
-def test_get_all_names():
-    x = get_all_names("tqdm")
-    assert "tqdm" in x
-    assert "trange" in x
-    x = get_all_names("examples.styles")
-    assert "ExampleClassGoogle" in x
-    assert "ExampleClassNumPy" in x
-
-
-def test_get_fullname():
-    src = "from collections.abc import Iterator"
-    node = ast.parse(src)
-    name, obj = _parse(node, "")[0]
-    assert name == "Iterator"
-    assert isinstance(obj, Object)
-    assert obj.module == "_collections_abc"
-    assert _get_fullname(obj) == "collections.abc.Iterator"
-
-
-def test_resolve_module_name():
-    name = "examples.styles.google"
-    x = resolve_module_name(name)
-    assert x == ("examples.styles.google", None)
-    name = "examples.styles.google.ExampleClass"
-    x = resolve_module_name(name)
-    assert x == ("examples.styles.google", "ExampleClass")
-    name = "examples.styles.ExampleClassGoogle"
-    x = resolve_module_name(name)
-    assert x == ("examples.styles.google", "ExampleClass")
-
-
-def test_resolve():
-    assert resolve("tqdm.tqdm") == "tqdm.std.tqdm"
-    assert resolve("jinja2.Template") == "jinja2.environment.Template"
-    assert resolve("mkdocs.config.Config") == "mkdocs.config.base.Config"
-    n = resolve("examples.styles.ExampleClassGoogle")
-    assert n == "examples.styles.google.ExampleClass"
-    assert resolve("mkapi.objects.ast") == "ast"
-    assert resolve("mkapi.objects.ast.ClassDef") == "ast.ClassDef"
-
-
-def test_resolve_from_module():
-    x = resolve_from_module("Class", "mkapi.objects")
-    assert x == "mkapi.objects.Class"
-    x = resolve_from_module("mkapi.objects", "mkapi.objects")
-    assert x == "mkapi.objects"
-    x = resolve_from_module("mkapi.objects.Class", "mkapi.objects")
-    assert x == "mkapi.objects.Class"
-
-    assert resolve_from_module("ast", "mkapi.objects") == "ast"
-    x = resolve_from_module("ast.ClassDef", "mkapi.objects")
-    assert x == "ast.ClassDef"
-
-    x = resolve_from_module("jinja2.Template", "mkdocs.plugins")
-    assert x == "jinja2.environment.Template"
-    x = resolve_from_module("jinja2.XXX", "mkdocs.plugins")
-    assert x == "jinja2.XXX"
-
-    for x in ["mkapi", "mkapi.ast", "mkapi.ast.XXX"]:
-        y = resolve_from_module(x, "mkapi.nodes")
-        assert x == y
-
-
-def test_resolve_from_module_qualname():
-    module = "examples.styles.google"
-    name = "ExampleClass"
-    assert resolve_from_module(name, module) == f"{module}.{name}"
-    name = "ExampleClass.attr1"
-    assert resolve_from_module(name, module) == f"{module}.{name}"
-    name = "ExampleClass.readonly_property"
-    assert resolve_from_module(name, module) == f"{module}.{name}"
-    name = "ExampleClass._private"
-    assert resolve_from_module(name, module) == f"{module}.{name}"
-
-    module = "examples.styles"
-    name = "ExampleClassGoogle"
-    x = resolve_from_module(name, module)
-    assert x == "examples.styles.google.ExampleClass"
-    name = "ExampleClassGoogle.readwrite_property"
-    x = resolve_from_module(name, module)
-    assert x == "examples.styles.google.ExampleClass.readwrite_property"
-
-
-# def test_split_module_name():
-#     name = "examples.styles.ExampleClassGoogle"
-#     for x in parse(name):
-#         print(x)
-#     assert 0
-
-
-def test_iter_decorator_names():
-    src = "@a(x)\n@b.c(y)\n@d\ndef f():\n pass"
-    node = ast.parse(src).body[0]
-    assert isinstance(node, ast.FunctionDef)
-    assert list(iter_decorator_names(node, "")) == ["a", "b.c", "d"]
-
-
-def test_get_decorator():
-    src = "@a(x)\n@b.c(y)\n@d\ndef f():\n pass"
-    node = ast.parse(src).body[0]
-    assert isinstance(node, ast.FunctionDef)
-    assert has_decorator(node, "d", "")
-    assert not has_decorator(node, "x", "")
+def test_a():
+    name = "polars"
+    # name = "mkdocs.plugins"
+    node = get_module_node(name)
+    assert node
+    x = parse(node, name)
+    for a in x:
+        print(a)
+    assert 0
