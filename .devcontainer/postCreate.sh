8428ebe9568fec73595bc4190991ea43adc0a36b
Daizu <daizutabi@gmail.com>
2024-04-08 07:27:46 +0900

    starship config

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 hatch config set dirs.env.virtual .hatch
-# echo 'eval "$(starship init zsh)"' >> ~/.zshrc
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
+mkdir -p ~/.config
+cp .devcontainer/starship.toml ~/.config
