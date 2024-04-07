5b7710ed65240cba06c2f1614d0a64efae40e3a7
Daizu <daizutabi@gmail.com>
2024-04-07 04:56:28 +0000

    Use bash

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 hatch config set dirs.env.virtual .hatch
-echo 'eval "$(starship init zsh)"' >> ~/.zshrc
+# echo 'eval "$(starship init zsh)"' >> ~/.zshrc
+echo 'eval "$(starship init bash)"' >> ~/.bashrc
