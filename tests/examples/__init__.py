be656445f91ae6e06a7ebed2acfc3dc1cb2f841a
Daizu <daizutabi@gmail.com>
2024-09-13 20:55:47 +0000

    Add examples

--- tests/examples/__init__.py
+++ tests/examples/__init__.py
@@ -1,1 +1,7 @@
 """Example package for testing MkAPI."""
+
+from examples import styles
+from examples.subpackage import module as M
+from examples.subpackage.module import A
+
+__all__ = ["styles", "M", "A"]
---
+++ tests/examples/subpackage/module.py
@@ -0,0 +1,12 @@
+"""A module"""
+
+
+class A:
+    """A class."""
+
+    def method(self):
+        """A method."""
+
+
+def f():
+    """A function."""
