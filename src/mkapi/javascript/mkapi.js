0300f2636cc3f093b853956a496524f0c2a1f2bf
Daizu <daizutabi@gmail.com>
2024-09-15 13:42:30 +0000

    Show/hide section contents

--- src/mkapi/javascript/mkapi.js
+++ src/mkapi/javascript/mkapi.js
@@ -1,6 +1,6 @@
-const buttons = document.querySelectorAll(".mkapi-parent-toggle");
+const parentButtons = document.querySelectorAll(".mkapi-parent-toggle");
 
-buttons.forEach((button) => {
+parentButtons.forEach((button) => {
   button.addEventListener("click", () => {
     const elements = document.querySelectorAll(".mkapi-object-parent");
     let isVisible = elements[0].style.display === "inline";
@@ -8,5 +8,15 @@ buttons.forEach((button) => {
     elements.forEach((element) => {
       element.style.display = isVisible ? "none" : "inline";
     });
+  });
+});
+
+const itemsButtons = document.querySelectorAll(".mkapi-section-toggle");
+
+itemsButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const element = button.parentElement.nextElementSibling;
+    let isInvisible = element.style.display === "none";
+    element.style.display = isInvisible ? "block" : "none";
   });
 });
