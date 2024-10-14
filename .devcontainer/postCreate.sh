0473c15e0f364cbf9537831386ee37f0babe926d
Daizu <daizutabi@gmail.com>
2024-10-14 06:24:05 +0000

    Use Python 3.13

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,5 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
 mkdir -p ~/.config
 cp .devcontainer/starship.toml ~/.config
+
+curl --proto '=https' --tlsv1.2 -LsSf https://github.com/astral-sh/uv/releases/download/0.4.20/uv-installer.sh | sh
+source $HOME/.cargo/env
+echo 'eval "$(uv generate-shell-completion bash)"' >> ~/.bashrc
