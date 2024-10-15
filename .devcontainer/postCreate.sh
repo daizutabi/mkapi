5976f461d25192e7cbe2cc1224284cb2ef6579ae
Daizu <daizutabi@gmail.com>
2024-10-15 09:39:50 +0000

    Update postCreate.sh

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -5,6 +5,6 @@ echo "alias ll='ls -alF'" >> ~/.bashrc
 mkdir -p ~/.config
 cp .devcontainer/starship.toml ~/.config
 
-curl --proto '=https' --tlsv1.2 -LsSf https://github.com/astral-sh/uv/releases/download/0.4.20/uv-installer.sh | sh
+curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.cargo/env
 echo 'eval "$(uv generate-shell-completion bash)"' >> ~/.bashrc
