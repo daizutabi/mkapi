4d89a94d9202cd0501a4b3f28a6107bffdb122a0
Daizu <daizutabi@gmail.com>
2025-01-14 21:48:34 +0000

    Update devcontainer

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -6,5 +6,6 @@ mkdir -p ~/.config
 cp .devcontainer/starship.toml ~/.config
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
+curl -LsSf https://astral.sh/ruff/install.sh | sh
 source $HOME/.cargo/env
 echo 'eval "$(uv generate-shell-completion bash)"' >> ~/.bashrc
